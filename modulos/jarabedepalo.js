f1a1294fc89dfc45e2da19a5aca0461ef779cf65
Eduar <educartoons@gmail.com>
2014-09-05 23:47:26 -0500

    La noche es bella

---
+++ modulos/jarabedepalo.js
@@ -0,0 +1,21 @@
+var age = 25;
+var state = 'single';
+
+function sing(){
+	console.log("bonito, todo me parece bonito");
+}
+
+function bonito(){
+	console.log("Sonreir! ! !");
+}
+
+function dormir(){
+	conole.log("ZZZzzz...");
+}
+
+module.exports = {
+	sing: sing,
+	bonito: bonito,
+	dormir: dormir,
+	state: state
+}
---
+++ modulos/popartists.js
@@ -0,0 +1,18 @@
+var magodeoz = require('./mymodule');
+var jarabedepalo = require('./jarabedepalo');
+
+module.exports = {
+	magodeoz: magodeoz,
+	cafe_tacuba: {
+		name: "Cafe Tacuba",
+		songs: [
+			"Mi primer millon",
+			"Pasos de gigante"
+		],
+		events: [
+			"Uruguay",
+			"Argentina"
+		]
+	},
+	jarabedepalo: jarabedepalo
+}
