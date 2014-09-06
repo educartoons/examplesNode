48765e25b938ab0e243617b61dac384e8d2aebec
Eduar <educartoons@gmail.com>
2014-09-05 23:34:19 -0500

    Seguimos practicando

---
+++ modulos/mymodule.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "Mago de Oz",
+	songs:[
+		"Some where Over the Rainbow",
+		"La Rosa de los Vientos",
+		"Pensando en ti"
+	],
+	events:[
+		"Peru",
+		"Colombia",
+		"Brasil"
+	]
+};
